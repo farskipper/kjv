9189aa69000308a8e77053f292af4e08d087250d
Matthew Wright <matthew@smallhelm.com>
2017-03-23 14:45:46 -0500

    importing 1769

---
+++ sources/1769/build.js
@@ -0,0 +1,52 @@
+var _ = require("lodash");
+var fs = require("fs");
+var path = require("path");
+var lexed = require("./lex");
+
+var layout = [];
+var verses = {};
+
+var curr_book;
+var curr_chap;
+var ref;
+var o;
+var i;
+for(i = 0; i < lexed.length; i++){
+    o = lexed[i];
+    if(o[0] === "BOOK"){
+        curr_book = o[1];
+        layout.push(["BOOK", o[1]]);
+    }else if(o[0] === "CHAPTER"){
+        curr_chap = o[1];
+        layout.push(["CHAPTER", o[1]]);
+    }else if(o[0] === "PARAGRAPH"){
+        layout.push(["PARAGRAPH"]);
+    }else if(o[0] === "VERSE"){
+        ref = curr_book + " " + curr_chap + ":" + o[1];
+        if(_.has(verses, ref)){
+            throw new Error("already has: " + ref + "\n\n" + verses[ref] + "\n\n" + o[2]);
+        }
+        verses[ref] = o[2];
+        layout.push(["VERSE", ref]);
+    }else if(o[0] === "TXT"){
+        layout.push(["TXT", o[1]]);
+    }else{
+        throw new Error("Not Handled: " + o[0]);
+    }
+}
+
+var writeJSON = function(name, data){
+    var str = _.isArray(data)
+        ? "[\n" + _.map(data, function(v){
+            return JSON.stringify(v);
+        }).join(",\n") + "\n]"
+        : "{\n" + _.map(data, function(v, k){
+            return JSON.stringify(k) + ":" + JSON.stringify(v);
+        }).join(",\n") + "\n}"
+        ;
+    var file = path.resolve(__dirname, "../../json/" + name);
+    fs.writeFileSync(file, str, "utf8");
+};
+
+writeJSON("layout-1769.json", layout);
+writeJSON("verses.json", verses);
---
+++ sources/1769/lex.js
@@ -0,0 +1,115 @@
+var _ = require("lodash");
+var fs = require("fs");
+
+var toInt = function(str){
+    return _.parseInt(str, 10) || 0;
+};
+
+var books = {
+    "THE FIRST BOOK OF MOSES, CALLED @ GENESIS": "Genesis",
+    "THE SECOND BOOK OF MOSES, CALLED @ EXODUS": "Exodus",
+    "THE THIRD BOOK OF MOSES, CALLED @ LEVITICUS": "Leviticus",
+    "THE FOURTH BOOK OF MOSES, CALLED @ NUMBERS": "Numbers",
+    "THE FIFTH BOOK OF MOSES, CALLED @ DEUTERONOMY": "Deuteronomy",
+    "THE BOOK OF @ JOSHUA": "Joshua",
+    "THE BOOK OF @ JUDGES": "Judges",
+    "THE BOOK OF @ RUTH": "Ruth",
+    "THE FIRST BOOK OF @ SAMUEL @ OTHERWISE CALLED, THE FIRST BOOK OF THE KINGS": "1 Samuel",
+    "THE SECOND BOOK OF @ SAMUEL @ OTHERWISE CALLED, THE SECOND BOOK OF THE KINGS": "2 Samuel",
+    "THE FIRST BOOK OF THE @ KINGS @ COMMONLY CALLED, THE THIRD BOOK OF THE KINGS": "1 Kings",
+    "THE SECOND BOOK OF THE @ KINGS @ COMMONLY CALLED, THE FOURTH BOOK OF THE KINGS": "2 Kings",
+    "THE FIRST BOOK OF THE @ CHRONICLES": "1 Chronicles",
+    "THE SECOND BOOK OF THE @ CHRONICLES": "2 Chronicles",
+    "EZRA": "Ezra",
+    "THE BOOK OF @ NEHEMIAH": "Nehemiah",
+    "THE BOOK OF @ ESTHER": "Esther",
+    "THE BOOK OF @ JOB": "Job",
+    "THE BOOK OF PSALMS": "Psalms",
+    "THE @ PROVERBS": "Proverbs",
+    "ECCLESIASTES @ OR, THE PREACHER": "Ecclesiastes",
+    "THE @ SONG OF SOLOMON": "Solomon's Song",
+    "THE BOOK OF THE PROPHET @ ISAIAH": "Isaiah",
+    "THE BOOK OF THE PROPHET @ JEREMIAH": "Jeremiah",
+    "THE @ LAMENTATIONS @ OF JEREMIAH": "Lamentations",
+    "THE BOOK OF THE PROPHET @ EZEKIEL": "Ezekiel",
+    "THE BOOK OF @ DANIEL": "Daniel",
+    "HOSEA": "Hosea",
+    "JOEL": "Joel",
+    "AMOS": "Amos",
+    "OBADIAH": "Obadiah",
+    "JONAH": "Jonah",
+    "MICAH": "Micah",
+    "NAHUM": "Nahum",
+    "HABAKKUK": "Habakkuk",
+    "ZEPHANIAH": "Zephaniah",
+    "HAGGAI": "Haggai",
+    "ZECHARIAH": "Zechariah",
+    "MALACHI": "Malachi",
+    "THE GOSPEL ACCORDING TO @ SAINT MATTHEW": "Matthew",
+    "THE GOSPEL ACCORDING TO @ SAINT MARK": "Mark",
+    "THE GOSPEL ACCORDING TO @ SAINT LUKE": "Luke",
+    "THE GOSPEL ACCORDING TO @ SAINT JOHN": "John",
+    "THE @ ACTS @ OF THE APOSTLES": "Acts",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ ROMANS": "Romans",
+    "THE FIRST EPISTLE OF PAUL THE APOSTLE TO THE @ CORINTHIANS": "1 Corinthians",
+    "THE SECOND EPISTLE OF PAUL THE APOSTLE TO THE @ CORINTHIANS": "2 Corinthians",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ GALATIANS": "Galatians",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ EPHESIANS": "Ephesians",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ PHILIPPIANS": "Philippians",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ COLOSSIANS": "Colossians",
+    "THE FIRST EPISTLE OF PAUL THE APOSTLE TO THE @ THESSALONIANS": "1 Thessalonians",
+    "THE SECOND EPISTLE OF PAUL THE APOSTLE TO THE @ THESSALONIANS": "2 Thessalonians",
+    "THE FIRST EPISTLE OF PAUL THE APOSTLE TO @ TIMOTHY": "1 Timothy",
+    "THE SECOND EPISTLE OF PAUL THE APOSTLE TO @ TIMOTHY": "2 Timothy",
+    "THE EPISTLE OF PAUL TO @ TITUS": "Titus",
+    "THE EPISTLE OF PAUL TO @ PHILEMON": "Philemon",
+    "THE EPISTLE OF PAUL THE APOSTLE TO THE @ HEBREWS": "Hebrews",
+    "THE GENERAL EPISTLE OF @ JAMES": "James",
+    "THE FIRST EPISTLE GENERAL OF @ PETER": "1 Peter",
+    "THE SECOND EPISTLE GENERAL OF @ PETER": "2 Peter",
+    "THE FIRST GENERAL EPISTLE OF @ JOHN": "1 John",
+    "THE SECOND EPISTLE OF @ JOHN": "2 John",
+    "THE THIRD EPISTLE OF @ JOHN": "3 John",
+    "THE GENERAL EPISTLE OF @ JUDE": "Jude",
+    "THE @ REVELATION @ OF SAINT JOHN THE DIVINE": "Revelation",
+};
+
+var out = [];
+var lines = fs.readFileSync("./THB_KJV_1769_AV_TEXT.txt", "utf8").split("\n");
+
+var started_first_book = false;
+
+var line;
+var i;
+for(i=0; i<lines.length; i++){
+    line = lines[i].trim();
+    if(line.length === 0){
+        continue;
+    }
+    if(!started_first_book){
+        if(_.has(books, line)){
+            started_first_book = true;
+        }else{
+            out.push(["TXT", line]);
+            continue;
+        }
+    }
+
+    var m_chap = /^(CHAPTER|PSALM) ([0-9]+)$/.exec(line);
+    var m_verse = /^([0-9]+) (.*)$/.exec(line);
+    if(_.has(books, line)){
+        out.push(["BOOK", books[line]]);
+    }else if(m_chap){
+        out.push(["CHAPTER", toInt(m_chap[2])]);
+    }else if(m_verse){
+        var text = m_verse[2].trim();
+        if(text[0] === "#"){
+            out.push(["PARAGRAPH"]);
+        }
+        out.push(["VERSE", toInt(m_verse[1]), text]);
+    }else{
+        out.push(["TXT", line]);
+    }
+}
+
+module.exports = out;
